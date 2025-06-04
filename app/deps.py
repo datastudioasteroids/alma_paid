2e1d961fce754f9b4d7ed86656d209b53f80b494
Data Studio Asteroids <heraclito09@gmail.com>
2025-06-04 07:11:32 -0300

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,24 +1,10 @@
-# app/deps.py
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from .database import Base
 from fastapi import Request
 from fastapi.responses import RedirectResponse
-
-# Ruta a tu archivo SQLite (puede ser relativa o absoluta)
-DATABASE_URL = "sqlite:///./alma_paid.db"
-
-# Crear el engine y el SessionLocal
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+from .database import SessionLocal
 
 def get_db():
     """
-    Abre una sesión de DB, la cede (yield) para que FastAPI la injete
+    Abre una sesión de BD, la cede (yield) para que FastAPI la injete
     en dependencias, y la cierra al finalizar la petición.
     """
     db = SessionLocal()
@@ -27,12 +13,11 @@ def get_db():
     finally:
         db.close()
 
-
 def ensure_admin(request: Request):
     """
     Comprueba si existe `request.session['admin']`.
     - Si no existe, redirige a /login.
-    - Si existe, devuelve el valor (normalmente el email del admin).
+    - Si existe, devuelve ese valor (por ejemplo, el email o ID del admin).
     """
     admin_user = request.session.get("admin")
     if not admin_user:
