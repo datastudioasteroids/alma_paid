3b12059da609066946aa14a40bf811f14abc8a9b
Data Studio Asteroids <heraclito09@gmail.com>
2025-06-04 07:11:00 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,4 +1,3 @@
-# app/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -12,7 +11,7 @@ engine = create_engine(
     connect_args={"check_same_thread": False}
 )
 
-# SessionLocal será la fábrica de sesiones
+# SessionLocal será la “fábrica” de sesiones que usarán todos los routers
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base para declarar modelos
